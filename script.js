5f84e7c5440e377c10a617131bd9bc2600955bbf
Ahmad Hamido <letraingrand1@gmail.com>
2022-11-09 12:49:56 +0100

    Fixed the addItem function and added the removeItem function

--- script.js
+++ script.js
@@ -1,30 +1,100 @@
 import { menuArray } from './data.js';
 
+const containerOrder = document.getElementById('container-order');
+
+let yourOrderArray = [];
+
 document.addEventListener("click", function(e) {
     if(e.target.dataset.item) {
         handleAddItemClick(e.target.dataset.item);
     }
+    else if (e.target.id === 'remove-item-btn') {
+        handleRemoveItemBtn(e.target.dataset.delete);
+    }
 })
-
-
 
 function handleAddItemClick(foodId) {
     const targetFoodObject = menuArray.filter(function(food) {
         return food.id === parseInt(foodId);
     })[0];
 
-    let total = 0;
+    yourOrderArray.push({
+        name: targetFoodObject.name,
+        price: targetFoodObject.price,
+        id: targetFoodObject.id
+    })
 
-    document.getElementById('order-list').innerHTML += `
-        <p class="order-item">${targetFoodObject.name}
-        <button class="remove-item-btn" id="remove-item-btn" data-itemRem="${targetFoodObject.id}">remove</button>
-        </p>
-        <p class="order-item-price">$${targetFoodObject.price}</p>
-    `
+    let orderList = '';
 
-    total += targetFoodObject.price;
+    yourOrderArray.forEach(function(order) {
+        orderList += `
+            <li class="list" id="list-${order.id}">
+                <div class="order" id="order">
+                    <p class="order-item">${order.name}
+                    <button class="remove-item-btn" id="remove-item-btn" data-delete="${order.id}">remove</button>
+                    </p>
+                </div>
+                <p class="order-item-price">$${order.price}</p>
+            </li>
+        `
+    })
 
-    document.getElementById('total-price').textContent += `$${total}`
+    document.getElementById('order-list').innerHTML = orderList;
+    
+
+    let totalPrice = 0;
+
+    yourOrderArray.forEach(function(order) {
+        totalPrice += order.price;
+    })
+
+    document.getElementById('total-price').textContent = 'Total Price: ' + '$' + totalPrice;
+
+    containerOrder.classList.remove('hidden');
+    render();
+}
+
+function handleRemoveItemBtn(orderId) {
+    const deleteOrderObject = yourOrderArray.filter(function(order) {
+        return order.id === parseInt(orderId);
+    })[0];
+    
+    for (let i = 0; i < yourOrderArray.length; i++) {
+        if (yourOrderArray[i] === deleteOrderObject) {
+            yourOrderArray.splice(i, 1);
+        }
+    }
+
+    let totalPrice = 0;
+
+    yourOrderArray.forEach(function(order) {
+        totalPrice += order.price;
+    })
+    
+    document.getElementById('total-price').textContent = 'Total Price: ' + '$' + totalPrice;
+
+    let orderList = '';
+
+    yourOrderArray.forEach(function(order) {
+        orderList += `
+            <li class="list" id="list-${order.id}">
+                <div class="order" id="order">
+                    <p class="order-item">${order.name}
+                    <button class="remove-item-btn" id="remove-item-btn" data-delete="${order.id}">remove</button>
+                    </p>
+                </div>
+                <p class="order-item-price">$${order.price}</p>
+            </li>
+        `
+    })
+
+    document.getElementById('order-list').innerHTML = orderList;
+    
+    if (yourOrderArray.length === 0) {
+        containerOrder.classList.add('hidden');
+    }
+
+    render();
 }
 
 function getHtml() {
